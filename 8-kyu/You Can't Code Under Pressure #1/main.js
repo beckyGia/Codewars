bf61a2480e29d238069d10a6495fdc73cc9169df
Becky Gia <b.adeyinka@hotmail.com>
2022-05-10 15:18:48 -0400

    Complete Codewars Challenge: #4

---
+++ 8-kyu/You Can't Code Under Pressure #1/main.js
@@ -0,0 +1,16 @@
+/*
+Title:
+You Can't Code Under Pressure #1
+
+Description:
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+
+//My Solution:
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  i = 2 * i;
+  return i;
+}
