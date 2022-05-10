670d5993a01266a43f7eb935377bf5fd6c15292b
Becky Gia <b.adeyinka@hotmail.com>
2022-05-10 14:38:21 -0400

    Completed Codewars Challenge: #2

---
+++ 8-kyu/Multiply/main.js
@@ -0,0 +1,18 @@
+/*
+Title:
+Multiply
+
+Description:
+This code does not execute properly. Try to figure out why.
+
+multiply = function (a, b) {
+ a * b;
+}
+*/
+
+
+//My Solution:
+
+multiply = function (a, b) {
+  return a * b;
+}
