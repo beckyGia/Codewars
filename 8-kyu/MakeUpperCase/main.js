84c584ed17920bdb0692fe71c7ff2d82f6be62b1
Becky Gia <b.adeyinka@hotmail.com>
2022-05-11 12:40:50 -0400

    Completed Codewars Challenge: #10

---
+++ 8-kyu/MakeUpperCase/main.js
@@ -0,0 +1,16 @@
+/*
+Title:
+MakeUpperCase
+
+Description:
+Write a function which converts the input string to uppercase.
+
+*/
+
+
+//My Solution:
+
+function makeUpperCase(str) {
+  // Code here
+  return str.toUpperCase();
+}
