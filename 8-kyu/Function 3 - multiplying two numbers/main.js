ca80f336e2d5c49bc44519715cb4734c30aef9c8
Becky Gia <b.adeyinka@hotmail.com>
2022-05-11 12:48:23 -0400

    Completed Codewars Challenge: #12

---
+++ 8-kyu/Function 3 - multiplying two numbers/main.js
@@ -0,0 +1,18 @@
+/*
+Title:
+Function 3 - multiplying two numbers
+
+Description:
+Implement a function which multiplies two numbers.
+
+*/
+
+
+//My Solution:
+
+// Write here your multiply-function
+function multiply(num1,num2) {
+  return num1 * num2;
+}
+
+multiply(2,3);
