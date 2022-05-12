af40330b51e0d8b1a9fd7906aecfa25aa104b512
Becky Gia <b.adeyinka@hotmail.com>
2022-05-12 18:49:53 -0400

    Completed Codewars Challenge: #24

---
+++ 8-kyu/Basic Training: Add item to an Array/main.js
@@ -0,0 +1,18 @@
+/*
+Title:
+Basic Training: Add item to an Array
+
+Description:
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+
+*/
+
+
+//My Solution:
+
+websites[0] = "codewars";
