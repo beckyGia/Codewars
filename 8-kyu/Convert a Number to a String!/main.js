dca798789d8decb2ec5fc4e5a9fc7c629bc6492d
Becky Gia <b.adeyinka@hotmail.com>
2022-05-12 18:24:27 -0400

    Completed Codewars Challenge: #18

---
+++ 8-kyu/Convert a Number to a String!/main.js
@@ -0,0 +1,22 @@
+/*
+Title:
+Convert a Number to a String!
+
+Description:
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+123 --> "123"
+999 --> "999"
+
+*/
+
+
+//My Solution:
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return num.toString();
+}
