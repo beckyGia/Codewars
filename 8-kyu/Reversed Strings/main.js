608b99ba0ac42220815f0ab1e055d1c1ddf33a06
Becky Gia <b.adeyinka@hotmail.com>
2022-05-12 18:32:41 -0400

    Completed Codewars Challenge: #19

---
+++ 8-kyu/Reversed Strings/main.js
@@ -0,0 +1,18 @@
+/*
+Title:
+Reversed Strings
+
+Description:
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+*/
+
+
+//My Solution:
+
+function solution(str){
+  return str.split("").reverse().join("");
+}
