de7170fd25b774581876c99238c088dfb2ed2a2e
Becky Gia <b.adeyinka@hotmail.com>
2022-05-13 13:01:28 -0400

    Completed Codewars Challenge: #47

---
+++ 8-kyu/Convert a Boolean to a String/main.js
@@ -0,0 +1,17 @@
+/*
+Title:
+Convert a Boolean to a String
+
+Description:
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+*/
+
+
+//My Solution:
+
+function booleanToString(b){
+  return b.toString();
+}
