ef6fac8341e3f83d72a8833825e0e5cc36769c65
Becky Gia <b.adeyinka@hotmail.com>
2022-05-16 23:49:03 -0600

    Completed Codewars Challenge: #58

---
+++ 8-kyu/Invert values/main.js
@@ -0,0 +1,22 @@
+/*
+Title:
+Invert values
+
+Description:
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+
+You can assume that all values are integers. Do not mutate the input array/list.
+
+*/
+
+
+//My Solution:
+
+function invert(array) { 
+  let arr1 = array.map(x => -x)
+    return arr1;
+}
