35bdd81d37f9981b1be186b046f7dd7dd2ef5fe7
Becky Gia <b.adeyinka@hotmail.com>
2022-05-20 22:58:29 -0600

    Completed Codewars Challenge: #62

---
+++ 8-kyu/Reversed Words/main.js
@@ -0,0 +1,21 @@
+/*
+Title:
+Reversed Words
+
+Description:
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Example:
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+}
+
+*/
+
+
+//My Solution:
+
+function reverseWords(str){
+  return str.split(" ").reverse().join(" ");; 
+}
