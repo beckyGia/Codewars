446edbc508a12cf9bd1cf4961dde538fa10947dd
Becky Gia <b.adeyinka@hotmail.com>
2022-06-18 19:35:04 -0600

    Completed Codewars Challenge: #91

---
+++ 8-kyu/Thinkful - Logic Drills: Traffic light/main.js
@@ -0,0 +1,31 @@
+/*
+Title:
+Thinkful - Logic Drills: Traffic light
+
+Description:
+You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+For example, when the input is green, output should be yellow.
+
+*/
+
+
+//My Solution:
+
+function updateLight(current) {
+  let future;
+  switch (current) {
+      case "green":
+        future = 'yellow';
+        break;
+      case "yellow":
+        future = 'red';
+        break;
+      case "red":
+        future = 'green';
+        break;
+  }
+  return future
+}
