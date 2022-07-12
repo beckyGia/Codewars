02b00806002fe9082859de33dee66be42a9500be
Becky Gia <b.adeyinka@hotmail.com>
2022-07-12 09:13:58 -0600

    Completed Codewars Challenge: #115

---
+++ 7-kyu/String ends with?/main.js
@@ -0,0 +1,16 @@
+/*
+Title:
+Shortest Word
+
+Description:
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+
+*/
+
+//My Solution:
+
+function solution(str, ending) {
+  return str.endsWith(ending);
+}
